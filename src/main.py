464d07d62af3f185738e7f714b3c59a03d8d34f8
ruan fab <victor.ruanfab@gmail.com>
2024-05-02 23:22:36 -0300

    adicionando schedule e fazendo a notificação do windows aparecer

--- src/main.py
+++ src/main.py
@@ -1,14 +1,8 @@
 import sys
 import time
-from win11toast import toast
 from setar_alarme import Alarm_config
 
-def send_notification(title: str, message: str):
-    toast(
-        title,
-        message,
-        app_id = 'despertador python'
-    )
+
 
 # print(f'alarme definido para {datetime.strptime(setting_alarm(), "%d-%m-%Y%H:%M:%S").strftime("%d-%m-%Y as %H:%M:%S")}')
 
--- src/setar_alarme.py
+++ src/setar_alarme.py
@@ -1,8 +1,19 @@
 from datetime import datetime
 import time
+import schedule
+from win11toast import toast
 
 current_time = datetime.now()
 day_print = current_time.strftime("%d-%m-%Y")
+
+
+def send_notification(title: str, message: str):
+    toast(
+        title,
+        message,
+        app_id = 'despertador python'
+    )
+
 
 class Alarm_config:
     def __init__(self):
@@ -31,9 +42,22 @@ class Alarm_config:
             if not isinstance(self.minute, int) or self.minute > 60 or self.minute < 0:
                 print('minuto tem que ser um numero por exemplo 30, e tem que ser maior que 0 e menor que 60')
             else:
-                minuto = str(self.minute) + ':'
+                minuto = str(self.minute)
                 valid_minute = True
-        self.time = day_print + hora + minuto + datetime.now().strftime('%S')
-        print(f'alarme definido para {datetime.strptime(self.time, "%d-%m-%Y%H:%M:%S").strftime("%d-%m-%Y as %H:%M:%S")}')
+        self.time = day_print + hora + minuto + ':' + datetime.now().strftime('%S')
 
-        
+        title_alarm = input('título do alarme: ')
+
+
+        print(f'\nalarme {title_alarm} está definido para {datetime.strptime(self.time, "%d-%m-%Y%H:%M:%S").strftime("%d-%m-%Y as %H:%M:%S")}')
+
+
+        # print(hora + minuto)
+        def calling_notification():
+            send_notification(title=title_alarm, message='seu alarme está disparando ' + hora + minuto)
+
+        schedule.every().day.at(hora + minuto).do(calling_notification)
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+
