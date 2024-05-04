80308c5e27d64ce908d385783409f41fb3b8393d
unknown <you@example.com>
2024-05-04 01:31:44 -0300

    finalizado

--- src/main.py
+++ src/main.py
@@ -1,10 +1,5 @@
-import sys
-import time
 from setar_alarme import Alarm_config
 
-
-
-# print(f'alarme definido para {datetime.strptime(setting_alarm(), "%d-%m-%Y%H:%M:%S").strftime("%d-%m-%Y as %H:%M:%S")}')
 
 
 alarme = Alarm_config()
--- src/setar_alarme.py
+++ src/setar_alarme.py
@@ -25,34 +25,43 @@ class Alarm_config:
         print('A que horas deseja pôr o alarme? (por exemplo 08:20)\n')
 
         valid_hour = False
+
         while valid_hour == False:
             self.hour = input('Hora: ')
+
 
             if int(self.hour) > 25 or int(self.hour) < 0:
                 print('Hora tem que ser um numero por exemplo 08, e tem que ser maior que 0 e menor que 24')
             else:
-                hora = str(self.hour) + ':'
+                if int(self.hour) < 10:
+                    hora = '0' + str(self.hour) + ':'
+                else:
+                    hora = str(self.hour) + ':'
+
                 valid_hour = True
 
-
         valid_minute = False
+
         while not valid_minute:
             self.minute = input('\nMinuto: ')
 
             if int(self.minute) > 60 or int(self.minute) < 0:
                 print('minuto tem que ser um numero por exemplo 30, e tem que ser maior que 0 e menor que 60')
             else:
-                minuto = str(self.minute)
+                if int(self.minute) < 10:
+                    minuto = '0' + str(self.minute)
+                else:
+                    minuto = str(self.minute)
+
                 valid_minute = True
         self.time = day_print + hora + minuto + ':' + datetime.now().strftime('%S')
 
         title_alarm = input('título do alarme: ')
 
 
-        print(f'\nalarme {title_alarm} está definido para {datetime.strptime(self.time, "%d-%m-%Y%H:%M:%S").strftime("%d-%m-%Y as %H:%M:%S")}')
+        print(f'\nalarme "{title_alarm}" está definido para {datetime.strptime(self.time, "%d-%m-%Y%H:%M:%S").strftime("%d-%m-%Y as %H:%M:%S")}')
 
 
-        # print(hora + minuto)
         def calling_notification():
             send_notification(title=title_alarm, message='seu alarme está disparando ' + hora + minuto)
 
@@ -60,4 +69,3 @@ class Alarm_config:
         while True:
             schedule.run_pending()
             time.sleep(1)
-
